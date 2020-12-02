a27c7cd5f9d84303c46df2860a1e0a191967a922
lewyk <lewyk@hotmail.com>
2020-12-02 23:52:05 +0200

    вроде работает

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+import gallaryTemplate from "./templates/template.hbs";
+import menu from "./menu.json";
+
+const refs = {
+  menu: document.querySelector(".js-menu"),
+  themeSwitch: document.querySelector(".theme-switch__toggle"),
+  body: document.querySelector("body"),
+};
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+const theme = localStorage.getItem("theme");
+
+if (!theme) {
+  refs.body.classList.add(Theme.LIGHT);
+} else {
+  refs.body.classList.add(theme);
+}
+
+if (theme === "dark-theme") {
+  refs.themeSwitch.checked = true;
+}
+
+markupGallery(menu);
+
+function markupGallery(data) {
+  const markup = gallaryTemplate(data);
+  refs.menu.insertAdjacentHTML("beforeend", markup);
+}
+
+refs.themeSwitch.addEventListener("input", switchHendler);
+
+function switchHendler() {
+  refs.body.classList.toggle(Theme.DARK);
+  refs.body.classList.toggle(Theme.LIGHT);
+  localStorage.setItem("theme", refs.body.className);
+}
